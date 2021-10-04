470495b94d13191ce70f1afbf7441acd2fee2545
splincode <splincode@users.noreply.github.com>
2021-10-04 16:46:03 +0300

    feat(addon-doc): support Promise content

--- projects/addon-doc/src/tokens/example-content-processor.ts
+++ projects/addon-doc/src/tokens/example-content-processor.ts
@@ -1,8 +1,7 @@
 import {InjectionToken} from '@angular/core';
 import {identity, TuiHandler} from '@taiga-ui/cdk';
+import {TuiDocExampleProcessed} from '../interfaces/page';
 
-export const TUI_DOC_EXAMPLE_CONTENT_PROCESSOR = new InjectionToken<
-    TuiHandler<Record<string, string>, Record<string, string>>
->('Processes content in example', {
-    factory: () => identity,
-});
+export const TUI_DOC_EXAMPLE_CONTENT_PROCESSOR: InjectionToken<
+    TuiHandler<TuiDocExampleProcessed, TuiDocExampleProcessed>
+> = new InjectionToken('Processes content in example', {factory: () => identity});
